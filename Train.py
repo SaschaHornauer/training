14484cf19be922111cc13985a80b7ce6586ccee2
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-11-04 12:40:22 -0700

    Changed dropouts

--- Train.py
+++ Train.py
@@ -62,7 +62,8 @@ def iterate(net, loss_func, optimizer=None, input=None, truth=None, train=True):
 
     # Run backprop, gradient clipping
     loss.backward()
-    nnutils.clip_grad_norm(net.parameters(), 1.)
+    for param in net.parameters():
+        nnutils.clip_grad_norm([param], 1.)
 
     # Apply backprop gradients
     optimizer.step()
@@ -137,10 +138,6 @@ def main():
 
                 loss = iterate(net, loss_func=loss_func, optimizer=optimizer,
                                input=(camera, meta), truth=truth)
-                if epoch == 3:
-                    for param in net.parameters():
-                        if 'grad' in dir(param):
-                            print param.grad
 
                 # Logging Loss
                 train_loss.add(loss)
