683e9da57df97a50f5a284e7c16b5daefcf21239
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-11-05 11:58:14 -0800

    Found gradients

--- nets/eric/tinymodels/SqueezeNetTimeLSTM.py
+++ nets/eric/tinymodels/SqueezeNetTimeLSTM.py
@@ -91,26 +91,29 @@ class SqueezeNetTimeLSTM(nn.Module):  # pylint: disable=too-few-public-methods
             nn.ELU(inplace=True),
             nn.BatchNorm2d(16),
             nn.Dropout2d(p=0.25),
-            nn.Conv2d(16, 8, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
             nn.ELU(inplace=True),
-            nn.BatchNorm2d(8),
+            nn.BatchNorm2d(16),
         )
         self.lstm_encoder = nn.ModuleList([
-            nn.LSTM(16, 32, 1, batch_first=True)
+            nn.LSTM(32, 64, 1, batch_first=True)
         ])
         self.lstm_decoder = nn.ModuleList([
-            nn.LSTM(2, 32, 1, batch_first=True)
+            nn.LSTM(32, 64, 1, batch_first=True)
         ])
-        self.output_linear = nn.Sequential(nn.Dropout(p=1./3.),
-                                           nn.Linear(32, 24),
-                                           nn.ELU(inplace=True),
-                                           nn.BatchNorm1d(24),
-                                           nn.Dropout(p=1./3.),
-                                           nn.Linear(24, 16),
-                                           nn.ELU(inplace=True),
-                                           nn.BatchNorm1d(16),
-                                           nn.Linear(16, 2),
-                                           nn.Sigmoid()
+        self.post_lstm_linear = nn.Sequential(
+                                            nn.Dropout(p=1./3.),
+                                            nn.Linear(64, 32),
+                                            nn.ELU(inplace=True),
+                                            nn.BatchNorm1d(32),
+                                              )
+        self.output_linear = nn.Sequential(
+                                            nn.Dropout(p=1. / 3.),
+                                            nn.Linear(32, 16),
+                                            nn.ELU(inplace=True),
+                                            nn.BatchNorm1d(16),
+                                            nn.Linear(16, 2),
+                                            nn.Sigmoid()
                                            )
 
         for mod in self.pre_lstm_output.modules():
@@ -130,10 +133,10 @@ class SqueezeNetTimeLSTM(nn.Module):  # pylint: disable=too-few-public-methods
         batch_size = camera_data.size(0)
         net_output = camera_data.contiguous().view(-1, 6, 94, 168)
         net_output = self.pre_lstm_output(net_output)
-        net_output = net_output.contiguous().view(batch_size, -1, 16)
+        net_output = net_output.contiguous().view(batch_size, -1, 32)
         for lstm in self.lstm_encoder:
             net_output, last_hidden_cell = lstm(net_output)
-            last_hidden_cell = list(last_hidden_cell)
+            # last_hidden_cell = list(last_hidden_cell)
         # for lstm in   self.lstm_decoder:
         #     if last_hidden_cell:
         #         net_output = lstm(self.get_decoder_input(camera_data), last_hidden_cell)[0]
@@ -142,10 +145,10 @@ class SqueezeNetTimeLSTM(nn.Module):  # pylint: disable=too-few-public-methods
         #         net_output = lstm(net_output)[0]
 
         # Initialize the decoder sequence
-        init_input = Variable(torch.ones(batch_size, 1, 2) * 0.5)
+        init_input = Variable(torch.ones(batch_size, 1, 32) * 0.5)
         init_input = init_input.cuda() if self.is_cuda else init_input
         lstm_output, last_hidden_cell = self.lstm_decoder[0](init_input, last_hidden_cell)
-        init_input = self.output_linear(lstm_output.contiguous().squeeze(1)).unsqueeze(1)
+        init_input = self.post_lstm_linear(lstm_output.contiguous().squeeze(1)).unsqueeze(1)
 
         if (controls is not None): #and (not self.is_generating):
             for lstm in self.lstm_decoder:
@@ -155,16 +158,19 @@ class SqueezeNetTimeLSTM(nn.Module):  # pylint: disable=too-few-public-methods
                 else:
                     net_output = lstm(net_output)[0]
             # net_output = last_hidden_cell[0]
-            net_output = self.output_linear(net_output.contiguous().view(-1, 32))
+            net_output = self.output_linear(self.post_lstm_linear(net_output.contiguous().view(-1, 64)))
         else:
             list_outputs = []
+            list_lstm_inputs = []
             for lstm in self.lstm_decoder:
                 for i in range(self.n_steps):
                     if i == 0:
                         lstm_output, last_hidden_cell = lstm(init_input, last_hidden_cell)
                     else:
-                        lstm_output, last_hidden_cell = lstm(list_outputs[i-1], last_hidden_cell)
-                    linear = self.output_linear(lstm_output.contiguous().view(-1, 32))
+                        lstm_output, last_hidden_cell = lstm(list_lstm_inputs[i-1], last_hidden_cell)
+                    linear = self.post_lstm_linear(lstm_output.contiguous().view(-1, 64))
+                    list_lstm_inputs.append(linear.unsqueeze(1))
+                    linear = self.output_linear(linear)
                     list_outputs.append(linear.unsqueeze(1))
             net_output = torch.cat(list_outputs, 1)
         # net_output = self.output_linear(net_output.contiguous().view(-1, 64))
