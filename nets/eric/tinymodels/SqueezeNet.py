39b547e4fa8c3cc4bf8a92dcbef43122f10ae8ad
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-11-08 08:26:56 -0800

    6FPS

--- nets/eric/tinymodels/SqueezeNet.py
+++ nets/eric/tinymodels/SqueezeNet.py
@@ -17,7 +17,7 @@ class Fire(nn.Module):
         super(Fire, self).__init__()
         self.final_output = nn.Sequential(
             torch.nn.BatchNorm2d(expand1x1_planes + expand3x3_planes),
-            nn.Dropout2d(p=0.2)
+            nn.Dropout2d(p=1./16.)
         )
         self.inplanes = inplanes
         self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
@@ -69,6 +69,7 @@ class SqueezeNet(nn.Module):
             pool(kernel_size=3, stride=2, ceil_mode=True),
             Fire(32, 16, 16, 16),
             Fire(32, 24, 24, 24),
+            nn.Dropout2d(0.3),
             Fire(48, 24, 24, 24),
             Fire(48, 32, 32, 32),
             pool(kernel_size=3, stride=2, ceil_mode=True),
